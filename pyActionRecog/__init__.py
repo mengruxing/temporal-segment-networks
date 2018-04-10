117b9ce22de332382c4cf5b77719fa2ba0897510
mengruxing <mengruxing@gmail.com>
2018-04-10 17:22:59 +0800

    支持 精简数据集

--- pyActionRecog/__init__.py
+++ pyActionRecog/__init__.py
@@ -5,6 +5,9 @@ split_parsers = dict()
 split_parsers['ucf101'] = parse_ucf_splits
 split_parsers['hmdb51'] = parse_hmdb51_splits
 split_parsers['jhmdb'] = parse_jhmdb_splits
+split_parsers['jhmdb_c15'] = parse_jhmdb_splits
+split_parsers['jhmdb_c10'] = parse_jhmdb_splits
+split_parsers['jhmdb_c5'] = parse_jhmdb_splits
 split_parsers['activitynet_1.2'] = lambda: parse_activitynet_splits("1.2")
 split_parsers['activitynet_1.3'] = lambda: parse_activitynet_splits("1.3")
 
